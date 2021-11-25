1c02722e1aac241877fe55b7b680a0d67174ce02
Laurent <laurent_baj@hotmail.com>
2021-11-25 15:14:17 +0100

    Added failing test

---
+++ src/test/java/com/pgr301/exam/BanckAccountApplicationTest.java
@@ -0,0 +1,13 @@
+package com.pgr301.exam;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class BanckAccountApplicationTest {
+
+    @Test
+    public void failedTest() {
+        fail();
+    }
+}
