72ae52bf3fa336055301f2b60e1a4d6484c15bc7
LaurentBaj <laurent_baj@hotmail.com>
2021-11-29 01:33:20 +0100

    fix2

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,4 +5,9 @@ terraform {
       version = "3.56.0"
     }
   }
+#  backend "s3" {
+#    bucket = "pgr301-laba006-terraform"
+#    key    = "global/s3/terraform.tfstate"
+#    region = "eu-west-1"
+#  }
 }
